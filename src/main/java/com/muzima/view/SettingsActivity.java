c3dd81578c2b4d177ab874aa5a9459a24fae19f1
Shashank Tomar <shashank004@gmail.com>
2013-08-12 14:29:26 +0530

    1) Fixed settings activity first launch null pointer issue, 2) Changed inputType for password field in settings activity to hide user input

--- src/main/java/com/muzima/view/SettingsActivity.java
+++ src/main/java/com/muzima/view/SettingsActivity.java
@@ -4,9 +4,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+
 import com.muzima.R;
 import com.muzima.search.api.util.StringUtil;
 
@@ -21,9 +22,9 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     private EditTextPreference passwordPreference;
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference);
 
         serverPreferenceKey = getResources().getString(R.string.preference_server);
@@ -36,11 +37,13 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
 
         passwordPreferenceKey = getResources().getString(R.string.preference_password);
         passwordPreference = (EditTextPreference) getPreferenceScreen().findPreference(passwordPreferenceKey);
-        passwordPreference.setSummary(passwordPreference.getText().replaceAll(".", "*"));
+        if (passwordPreference.getText() != null) {
+            passwordPreference.setSummary(passwordPreference.getText().replaceAll(".", "*"));
+        }
 
-		// Show the Up button in the action bar.
-		setupActionBar();
-	}
+        // Show the Up button in the action bar.
+        setupActionBar();
+    }
 
     /**
      * Called when a shared preference is changed, added, or removed. This
@@ -92,34 +95,34 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     }
 
     /**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-	}
+     * Set up the {@link android.app.ActionBar}.
+     */
+    private void setupActionBar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
