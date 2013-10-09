77613d69cca2244f0edc9d4ac7ad6fd1eb60af9c
Tibo <ttakado@thoughtworks.com>
2013-10-09 11:41:34 +0200

    Added navigation buttons in wizard screen to select cohorts

---
+++ src/main/java/com/muzima/view/cohort/CohortWizardActivity.java
@@ -0,0 +1,16 @@
+package com.muzima.view.cohort;
+
+import android.os.Bundle;
+import com.muzima.R;
+import com.muzima.view.BroadcastListenerActivity;
+
+
+public class CohortWizardActivity extends BroadcastListenerActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // synch cohort
+        // create fragment
+        // display fragment
+        setContentView(R.layout.activity_cohort_wizard);
+    }
+}
