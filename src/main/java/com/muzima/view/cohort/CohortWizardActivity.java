0ec9a6356baab3c4e527d125b0c8e16796b07c3f
Tibo <ttakado@thoughtworks.com>
2013-10-09 16:43:23 +0200

    Now can select all cohorts in wizard screen to select cohorts

--- src/main/java/com/muzima/view/cohort/CohortWizardActivity.java
+++ src/main/java/com/muzima/view/cohort/CohortWizardActivity.java
@@ -1,6 +1,8 @@
 package com.muzima.view.cohort;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import com.muzima.MuzimaApplication;
 import com.muzima.R;
@@ -12,9 +14,23 @@ public class CohortWizardActivity extends BroadcastListenerActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cohort_wizard);
-        ListView listView = (ListView) findViewById(R.id.cohort_wizard_list);
-        AllCohortsAdapter cohortsAdapter = new AllCohortsAdapter(getApplicationContext(), R.layout.item_cohorts_list, ((MuzimaApplication) getApplicationContext()).getCohortController());
+        ListView listView = getListView();
+        final AllCohortsAdapter cohortsAdapter = createAllCohortsAdapter();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cohortsAdapter.onListItemClick(position);
+            }
+        });
         cohortsAdapter.reloadData();
         listView.setAdapter(cohortsAdapter);
+    }
+
+    private AllCohortsAdapter createAllCohortsAdapter() {
+        return new AllCohortsAdapter(getApplicationContext(), R.layout.item_cohorts_list, ((MuzimaApplication) getApplicationContext()).getCohortController());
+    }
+
+    private ListView getListView() {
+        return (ListView) findViewById(R.id.cohort_wizard_list);
     }
 }
