2a989b9bb20fcd38c38a5c8c275d18f7944800e1
Tibo <ttakado@thoughtworks.com>
2013-10-09 14:57:49 +0200

    Now displays all available cohorts in wizard screen to select cohorts

--- src/main/java/com/muzima/view/cohort/CohortWizardActivity.java
+++ src/main/java/com/muzima/view/cohort/CohortWizardActivity.java
@@ -1,16 +1,20 @@
 package com.muzima.view.cohort;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import com.muzima.MuzimaApplication;
 import com.muzima.R;
+import com.muzima.adapters.cohort.AllCohortsAdapter;
 import com.muzima.view.BroadcastListenerActivity;
 
 
 public class CohortWizardActivity extends BroadcastListenerActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // synch cohort
-        // create fragment
-        // display fragment
         setContentView(R.layout.activity_cohort_wizard);
+        ListView listView = (ListView) findViewById(R.id.cohort_wizard_list);
+        AllCohortsAdapter cohortsAdapter = new AllCohortsAdapter(getApplicationContext(), R.layout.item_cohorts_list, ((MuzimaApplication) getApplicationContext()).getCohortController());
+        cohortsAdapter.reloadData();
+        listView.setAdapter(cohortsAdapter);
     }
 }
