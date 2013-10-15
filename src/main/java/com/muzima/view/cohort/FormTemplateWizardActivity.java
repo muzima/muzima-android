f724d6652ad25d93d6f76e5c5d908f378b761c70
Akani Maluleke <akanim@gmail.com>
2013-10-15 15:30:19 +0200

    Show tags in the download form templates wizard

--- src/main/java/com/muzima/view/cohort/FormTemplateWizardActivity.java
+++ src/main/java/com/muzima/view/cohort/FormTemplateWizardActivity.java
@@ -4,17 +4,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.muzima.MuzimaApplication;
 import com.muzima.R;
 import com.muzima.adapters.forms.AllAvailableFormsAdapter;
+import com.muzima.adapters.forms.FormsPagerAdapter;
+import com.muzima.adapters.forms.TagsListAdapter;
+import com.muzima.controller.FormController;
 import com.muzima.service.DataSyncService;
+import com.muzima.utils.Fonts;
 import com.muzima.view.BroadcastListenerActivity;
 import com.muzima.view.HelpActivity;
 import com.muzima.view.MainActivity;
@@ -28,9 +36,23 @@ import static com.muzima.utils.Constants.DataSyncServiceConstants.SYNC_TYPE;
 
 
 public class FormTemplateWizardActivity extends BroadcastListenerActivity {
+    private MenuItem tagsButton;
+    private DrawerLayout mainLayout;
+
+    private ListView tagsDrawerList;
+    private TextView tagsNoDataMsg;
+    private ActionBarDrawerToggle actionbarDrawerToggle;
+    private TagsListAdapter tagsListAdapter;
+    private MenuItem menubarLoadButton;
+    private FormController formController;
+
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_templates_wizard);
+        mainLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_form_templates_wizard, null);
+        setContentView(mainLayout);
+        formController = ((MuzimaApplication) getApplication()).getFormController();
         ListView listView = getListView();
         final AllAvailableFormsAdapter allAvailableFormsAdapter = createAllFormsAdapter();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -60,6 +82,8 @@ public class FormTemplateWizardActivity extends BroadcastListenerActivity {
                 navigateToPreviousActivity();
             }
         });
+
+        initDrawer();
     }
 
     private void navigateToPreviousActivity() {
@@ -84,10 +108,17 @@ public class FormTemplateWizardActivity extends BroadcastListenerActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        tagsListAdapter.reloadData();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
+        getSupportMenuInflater().inflate(R.menu.download_form_template_wizard, menu);
         MenuItem menuSettings = menu.findItem(R.id.action_settings);
         menuSettings.setEnabled(false);
+        tagsButton = menu.findItem(R.id.menu_tags);
         return true;
     }
 
@@ -98,6 +129,13 @@ public class FormTemplateWizardActivity extends BroadcastListenerActivity {
                 Intent intent = new Intent(this, HelpActivity.class);
                 intent.putExtra(HelpActivity.HELP_TYPE, HelpActivity.COHORT_WIZARD_HELP);
                 startActivity(intent);
+                return true;
+            case R.id.menu_tags:
+                if (mainLayout.isDrawerOpen(GravityCompat.END)) {
+                    mainLayout.closeDrawer(GravityCompat.END);
+                } else {
+                    mainLayout.openDrawer(GravityCompat.END);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -120,4 +158,42 @@ public class FormTemplateWizardActivity extends BroadcastListenerActivity {
         startService(intent);
     }
 
+    private void initDrawer() {
+        tagsDrawerList = (ListView) findViewById(R.id.tags_list);
+        tagsDrawerList.setEmptyView(findViewById(R.id.tags_no_data_msg));
+        tagsListAdapter = new TagsListAdapter(this, R.layout.item_tags_list, formController);
+        tagsDrawerList.setAdapter(tagsListAdapter);
+        tagsDrawerList.setOnItemClickListener(tagsListAdapter);
+//        tagsListAdapter.setTagsChangedListener((FormsPagerAdapter) formsPagerAdapter);
+        actionbarDrawerToggle = new ActionBarDrawerToggle(this, mainLayout,
+                R.drawable.ic_labels, R.string.drawer_open, R.string.drawer_close) {
+
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                String title = getResources().getString(R.string.title_activity_form_list);
+                getSupportActionBar().setTitle(title);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                mainLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            }
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                String title = getResources().getString(R.string.drawer_title);
+                getSupportActionBar().setTitle(title);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                mainLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            }
+        };
+        mainLayout.setDrawerListener(actionbarDrawerToggle);
+        mainLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+
+        tagsNoDataMsg = (TextView) findViewById(R.id.tags_no_data_msg);
+        tagsNoDataMsg.setTypeface(Fonts.roboto_bold_condensed(this));
+    }
+
 }
+
