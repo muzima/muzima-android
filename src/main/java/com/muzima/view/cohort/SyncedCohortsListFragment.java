29e453b5f3c7a7e7074da489683bd7dd2ddb95b8
Tibo <ttakado@thoughtworks.com>
2013-11-15 14:13:18 +0200

    Unselect all downloaded cohorts when a cohort is selected

--- src/main/java/com/muzima/view/cohort/SyncedCohortsListFragment.java
+++ src/main/java/com/muzima/view/cohort/SyncedCohortsListFragment.java
@@ -2,8 +2,11 @@ package com.muzima.view.cohort;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ListView;
 import com.muzima.R;
 import com.muzima.adapters.cohort.SyncedCohortsAdapter;
 import com.muzima.api.model.Cohort;
@@ -12,6 +15,7 @@ import com.muzima.view.patients.PatientsListActivity;
 
 public class SyncedCohortsListFragment extends CohortListFragment implements AllCohortsListFragment.OnCohortDataDownloadListener {
     private static final String TAG = "SyncedCohortsListFragment";
+    private ListView listView;
 
     public static SyncedCohortsListFragment newInstance(CohortController cohortController) {
         SyncedCohortsListFragment f = new SyncedCohortsListFragment();
@@ -31,11 +35,25 @@ public class SyncedCohortsListFragment extends CohortListFragment implements All
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        unselectAllItems(listView);
         Cohort cohort = (Cohort) listAdapter.getItem(position);
         Intent intent = new Intent(getActivity(), PatientsListActivity.class);
         intent.putExtra(PatientsListActivity.COHORT_ID, cohort.getUuid());
         intent.putExtra(PatientsListActivity.COHORT_NAME, cohort.getName());
         startActivity(intent);
+    }
+
+    private void unselectAllItems(ListView listView) {
+        for (int i = listView.getCount() - 1; i >= 0; i--){
+            listView.setItemChecked(i, false);
+        }
+    }
+
+    @Override
+    protected View setupMainView(LayoutInflater inflater, ViewGroup container) {
+        View view = inflater.inflate(R.layout.layout_synced_list, container, false);
+        listView = (ListView) view.findViewById(R.id.list);
+        return view;
     }
 
     @Override
