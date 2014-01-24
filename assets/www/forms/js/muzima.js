f4ca4face91d33f3226794ae2b3d6539bf3a5ba3
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2014-01-24 17:12:37 +0200

    Added CheckDigit Algo to the validator

--- assets/www/forms/js/muzima.js
+++ assets/www/forms/js/muzima.js
@@ -17,4 +17,76 @@ $(document).ready(function () {
 
     /*End- BarCode Functionality*/
 
+    /* Start - Serialise FormDate to JSON. With all name attrs being a key and value of input being a value */
+    $.fn.serializeForm = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+    /* End - Serialise FormDate to JSON. With all name attrs being a key and value of input being a value */
+
+    /* Start - Initialize jQuery DatePicker */
+
+    $('.datepicker').datepicker({
+        dateFormat: 'dd-M-yy',
+        changeMonth: true,
+        changeYear: true
+    });
+
+    /* Start - Initialize jQuery DatePicker */
+
+    /* Start - CheckDigit algorithm Source: https://wiki.openmrs.org/display/docs/Check+Digit+Algorithm */
+
+    $.validator.addMethod("checkDigit", function (value, element) {
+            var num = value.split('-');
+            if (num.length != 2) {
+                return false;
+            }
+            return $.fn.luhnCheckDigit(num[0]) == num[1];
+        }, "Please enter digits that matches CheckDigit algorithm."
+    );
+
+    // connect it to a css class
+    jQuery.validator.addClassRules({
+        checkDigit : { checkDigit : true }
+    });
+
+    $.fn.luhnCheckDigit = function (number) {
+        var validChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVYWXZ_";
+        number = number.toUpperCase().trim();
+        var sum = 0;
+        for (var i = 0; i < number.length; i++) {
+            var ch = number.charAt(number.length - i - 1);
+            if (validChars.indexOf(ch) < 0) {
+                alert("Invalid character(s) found!");
+                return false;
+            }
+            var digit = ch.charCodeAt(0) - 48;
+            var weight;
+            if (i % 2 == 0) {
+                weight = (2 * digit) - parseInt(digit / 5) * 9;
+            }
+            else {
+                weight = digit;
+            }
+            sum += weight;
+        }
+        sum = Math.abs(sum) + 10;
+        return (10 - (sum % 10)) % 10;
+    };
+
+    /* End - CheckDigit Algorithm */
+
+
 });
