4b85cdaced556871dbea5264b553335744d7b079
Tibo <ttakado@thoughtworks.com>
2014-02-27 14:51:02 +0200

    Corrected error in displayed text during cohort download

--- src/main/java/com/muzima/service/DataSyncService.java
+++ src/main/java/com/muzima/service/DataSyncService.java
@@ -80,7 +80,7 @@ public class DataSyncService extends IntentService {
                 updateNotificationMsg("Downloading Cohorts");
                 if (authenticationSuccessful(credentials, broadcastIntent)) {
                     int[] result = muzimaSyncService.downloadCohorts();
-                    String msg = "Downloaded " + result[1] + " new cohorts; " + "; and deleted " + result[2] + " cohorts";
+                    String msg = "Downloaded " + result[1] + " new cohorts" + "; and deleted " + result[2] + " cohorts";
                     prepareBroadcastMsg(broadcastIntent, result, msg);
                     saveCohortsSyncTime(result);
                     consolidateAndSyncIndependentPatients(broadcastIntent);
