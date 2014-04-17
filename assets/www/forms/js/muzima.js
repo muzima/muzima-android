9d907cc4e885daa3c1f7751ebb6b731ce2b1ffef
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2014-04-17 19:59:02 +0530

    Resetting the input element values to empty while cloning the element.

--- assets/www/forms/js/muzima.js
+++ assets/www/forms/js/muzima.js
@@ -211,7 +211,9 @@ $(document).ready(function () {
         .append("<input class='btn btn-primary remove_section pull-right' type='button' value='-'/><span class='clear'></span>");
 
     $(document.body).on('click', '.add_section', function () {
-        $(this).parent().clone().insertAfter($(this).parent());
+        var $clonedElement = $(this).parent().clone();
+        $clonedElement.find('input:not(:button)').val('');
+        $clonedElement.insertAfter($(this).parent());
     });
 
     $(document.body).on('click', '.remove_section', function () {
