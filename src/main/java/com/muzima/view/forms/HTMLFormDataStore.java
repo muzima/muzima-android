088888a262ba37a73fd6ea770e972c8468dcc711
vikaspandeysahaj <vikas.pandey06081986@gmail.com>
2014-09-24 17:30:45 +0530

    MUZIMA-157 : Prompt message when draft form data is saved successfully

--- src/main/java/com/muzima/view/forms/HTMLFormDataStore.java
+++ src/main/java/com/muzima/view/forms/HTMLFormDataStore.java
@@ -52,6 +52,9 @@ public class HTMLFormDataStore {
             Log.i(TAG, "Saving form data ...");
             if (!keepFormOpen) {
                 formWebViewActivity.finish();
+                if(status.equals("incomplete")) {
+                    Toast.makeText(formWebViewActivity, "Draft form data is saved successfully.", Toast.LENGTH_SHORT).show();
+                }
             }
         } catch (FormController.FormDataSaveException e) {
             Toast.makeText(formWebViewActivity, "An error occurred while saving the form", Toast.LENGTH_SHORT).show();
