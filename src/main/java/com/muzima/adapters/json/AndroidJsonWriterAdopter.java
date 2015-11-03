e9ff285575669365f47027673a126284c20c8b00
Win Ribeka <nyoman.ribeka@gmail.com>
2015-11-03 06:39:21 -0500

    Replacing Android JsonWriter with the one from org.json.

--- src/main/java/com/muzima/adapters/json/AndroidJsonWriterAdopter.java
+++ src/main/java/com/muzima/adapters/json/AndroidJsonWriterAdopter.java
@@ -8,79 +8,56 @@
 
 package com.muzima.adapters.json;
 
-import android.util.JsonWriter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import com.muzima.api.adapter.JsonWriterAdapter;
 import org.json.JSONException;
+import org.json.JSONWriter;
 
-import java.io.IOException;
 import java.io.Writer;
 
 // TODO: Need to look at this in the Android 8
 public class AndroidJsonWriterAdopter implements JsonWriterAdapter {
-    private JsonWriter jsonWriter;
+    private JSONWriter jsonWriter;
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public AndroidJsonWriterAdopter(Writer writer) {
-        this.jsonWriter = new JsonWriter(writer);
+        this.jsonWriter = new JSONWriter(writer);
     }
 
     @Override
     public JsonWriterAdapter array() throws JSONException {
-
-        try {
-            jsonWriter.beginArray();
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.array();
         return this;
     }
 
     @Override
     public JsonWriterAdapter object() throws JSONException {
-        try {
-            jsonWriter.beginObject();
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.object();
         return this;
     }
 
     @Override
     public JsonWriterAdapter key(String key) throws JSONException {
-        try {
-            jsonWriter.name(key);
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.key(key);
         return this;
     }
 
     @Override
     public JsonWriterAdapter value(String value) throws JSONException {
-        try {
-            jsonWriter.value(value);
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.value(value);
         return this;
     }
 
     @Override
     public JsonWriterAdapter endObject() throws JSONException {
-        try {
-            jsonWriter.endObject();
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.endObject();
         return this;
     }
 
     @Override
     public JsonWriterAdapter endArray() throws JSONException {
-        try {
-            jsonWriter.endArray();
-        } catch (IOException e) {
-            throw new JSONException(e.getMessage());
-        }
+        jsonWriter.endArray();
         return this;
     }
 }
