f0fd9838d8999bff2684ad49c79518a1d33f245b
mssavai <mssavai@gmail.com>
2016-10-04 13:52:16 +0300

    backport commit 8155e24

--- src/main/java/com/muzima/utils/StringUtils.java
+++ src/main/java/com/muzima/utils/StringUtils.java
@@ -44,4 +44,13 @@ public class StringUtils {
     public static String defaultString(String string) {
         return string == null ? "" : string;
     }
+
+    public static boolean equals(String str1, String str2) {
+        return str1 == null ? str2 == null : str1.equals(str2);
+    }
+
+    public static int nullSafeCompare(String str1, String str2){
+        return str1 == null ? str2 == null ? 0 : -1 :
+                str2 == null ? 1 : str1.compareTo(str2);
+    }
 }
