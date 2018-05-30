47b70cab4cc577c03b045de84f6a447dd08fbc4e
Patrick <gpatryllus@gmail.com>
2018-05-30 11:38:05 +0300

    Added unique patients: total tested, total positive, total linked,total contacts

--- app/src/main/java/com/muzima/view/forms/ProviderReportsFormActivity.java
+++ app/src/main/java/com/muzima/view/forms/ProviderReportsFormActivity.java
@@ -23,6 +23,7 @@ import com.muzima.MuzimaApplication;
 import com.muzima.R;
 import com.muzima.adapters.MuzimaPagerAdapter;
 import com.muzima.api.model.FormData;
+import com.muzima.api.model.Patient;
 import com.muzima.api.model.Provider;
 import com.muzima.utils.Constants;
 import com.muzima.controller.FormController.FormDataFetchException;
@@ -36,7 +37,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static android.webkit.ConsoleMessage.MessageLevel.ERROR;
 import static com.muzima.utils.Constants.FORM_DISCRIMINATOR_REGISTRATION;
@@ -46,7 +49,7 @@ import static java.text.MessageFormat.format;
 
 
 public class ProviderReportsFormActivity extends FormsActivityBase {
-    public static final String TAG = "ProviderReportsFormActivity";
+    public static final String TAG = "ProviderReportsActivity";
     public Provider provider;
     public FormController formController;
     private MuzimaProgressDialog progressDialog;
@@ -134,7 +137,7 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
         MuzimaApplication muzimaApplication = (MuzimaApplication) getApplication();
         FormController formController = muzimaApplication.getFormController();
 
-        List<FormData> htsFormData = new ArrayList<FormData>();
+        Set<String> testedPatients = new HashSet<String>();
         List<FormData> allFormData = null;
 
 
@@ -148,8 +151,10 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
                 org.json.JSONObject object = new org.json.JSONObject(formData.getJsonPayload());
                 String formUuid = ((org.json.JSONObject) object.get("encounter")).get("encounter.form_uuid").toString();
                if(formUuid.equals("84c21c60-1910-4e0a-8579-a303d94b6c7b")){                                                 // Selects HTS forms by UUID
-                   htsFormData.add(formData);
-                   count = htsFormData.size();
+
+                   String patientUuid = formData.getPatientUuid();
+                   testedPatients.add(patientUuid);
+                   count = testedPatients.size();
                }
             }
         }
@@ -168,7 +173,7 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
         MuzimaApplication muzimaApplication = (MuzimaApplication) getApplication();
         FormController formController = muzimaApplication.getFormController();
 
-        List<FormData> htsFormData = new ArrayList<FormData>();
+        Set<String> testedPositivePatients = new HashSet<String>();
         List<FormData> allFormData = null;
 
 
@@ -184,8 +189,10 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
                 if (formUuid.equals("84c21c60-1910-4e0a-8579-a303d94b6c7b")) {                                                 // Selects HTS forms by UUID
                     String finalHIVResult = ((org.json.JSONObject) object.get("observation")).get("159427^FINAL HIV RESULTS^99DCT").toString();
                     if (finalHIVResult.equals("703^POSITIVE^99DCT")) {
-                        htsFormData.add(formData);
-                        count = htsFormData.size();
+
+                        String patientUuid = formData.getPatientUuid();
+                        testedPositivePatients.add(patientUuid);
+                        count = testedPositivePatients.size();
                     }
                 }
 
@@ -206,7 +213,7 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
         MuzimaApplication muzimaApplication = (MuzimaApplication) getApplication();
         FormController formController = muzimaApplication.getFormController();
 
-        List<FormData> linkageFormData = new ArrayList<FormData>();
+        Set<String> linkedPatients = new HashSet<String>();
         List<FormData> allFormData = null;
 
 
@@ -222,8 +229,10 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
                 if (formUuid.equals("25a541fb-9126-4616-994e-e2b2b4fd4ca1")) {                                                 // Selects Linkage and referral forms by UUID
                     String linkedInCare = ((org.json.JSONObject) object.get("observation")).get("159811^ENROLLED IN HIV CARE^99DCT").toString();
                     if (linkedInCare.equals("1065^YES^99DCT")){
-                        linkageFormData.add(formData);
-                        count = linkageFormData.size();
+
+                        String patientUuid = formData.getPatientUuid();
+                        linkedPatients.add(patientUuid);
+                        count = linkedPatients.size();
                     }
                 }
 
@@ -237,13 +246,11 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
     public String totalContactsCountInterface() throws IOException, JSONException {
         return totalContactsCount().toString();
     }
-    //Total linked
-    public Integer totalContactsCount() throws IOException, JSONException {                                //Total linked
+    //Total contacts
+    public Integer totalContactsCount() throws IOException, JSONException {                                //Total contacts
         Integer count = 0;
         MuzimaApplication muzimaApplication = (MuzimaApplication) getApplication();
         FormController formController = muzimaApplication.getFormController();
-
-        List<FormData> contactsFormData = new ArrayList<FormData>();
         List<FormData> allFormData = null;
        // String[] contacts ={};
         try {
@@ -257,8 +264,8 @@ public class ProviderReportsFormActivity extends FormsActivityBase {
                 if (formUuid.equals("8ba71e39-bc0f-408a-9dbc-19982ffd0f14")) {                                                 // Selects contact listing forms by UUID
                    String contactsObservation = ((org.json.JSONObject) object.get("observation")).get("160593^Expected^99DCT").toString();
                     JSONArray jsonArray = new JSONArray(contactsObservation);
-                       count = jsonArray.getJSONObject(0).length();
-                        //System.out.println("array length is: "+count);/*the result is 1! */
+                       count = jsonArray.length();
+                        System.out.println("Total contacts: "+count);/*the result is 1! */
                     }
             }
         }
