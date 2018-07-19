aca88b26ac0ae09179340516d1f35be22414483f
samuel owino <samuelowino43@yahoo.com>
2018-07-19 11:51:51 +0300

    messaging

---
+++ app/src/main/java/com/muzima/adapters/MessageThreadAdapter.java
@@ -0,0 +1,107 @@
+package com.muzima.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.muzima.api.model.Notification;
+import com.muzima.MuzimaApplication;
+import com.muzima.R;
+import com.github.library.bubbleview.BubbleTextView;
+
+import java.util.List;
+
+public class MessageThreadAdapter extends BaseAdapter {
+
+    private List<Notification> chatModelList;
+    private Context context;
+    private LayoutInflater inflater;
+
+    public MessageThreadAdapter(List<Notification> chatModelList, Context context) {
+        this.chatModelList = chatModelList;
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    @Override
+    public int getCount() {
+        return chatModelList.size();
+    }
+
+    /**
+     * Get the data item associated with the specified position in the data set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
+    @Override
+    public Object getItem(int position) {
+        return chatModelList.get(position);
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param position The position of the item within the adapter's data set whose row id we want.
+     * @return The id of the item at the specified position.
+     */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void updateResults(List<Notification> results) {
+        notifyDataSetChanged();
+        chatModelList.addAll(results);
+        //Triggers the list update
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        String loggedInUserUuid = ((MuzimaApplication)convertView.getContext()).getAuthenticatedUser().getPerson().getUuid();
+        if (convertView == null) {
+            if (chatModelList.get(position).getSender().getUuid().equals(loggedInUserUuid)) {
+                view = inflater.inflate(R.layout.item_layout_send, null);
+                BubbleTextView bubbleTextView = view.findViewById(R.id.chat_textview);
+                bubbleTextView.setText(chatModelList.get(position).getPayload());
+            } else
+                view = inflater.inflate(R.layout.item_receive_layout, null);
+            BubbleTextView bubbleTextView = view.findViewById(R.id.chat_textview);
+            bubbleTextView.setText(chatModelList.get(position).getPayload());
+        }
+
+        BubbleTextView bubbleTextView = view.findViewById(R.id.chat_textview);
+        bubbleTextView.setText(chatModelList.get(position).getPayload());
+
+        return view;
+    }
+}
+
