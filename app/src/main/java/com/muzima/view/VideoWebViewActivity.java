956d240e3e98930a786417a65d079be03e6eb60c
Zita <zita.reiner@gmail.com>
2019-01-24 13:57:01 +0000

    webview changes - code cleaning

--- app/src/main/java/com/muzima/view/VideoWebViewActivity.java
+++ app/src/main/java/com/muzima/view/VideoWebViewActivity.java
@@ -10,28 +10,19 @@
 
 package com.muzima.view;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.webkit.*;
-import android.widget.Toast;
 import com.muzima.MuzimaApplication;
 import com.muzima.R;
 import com.muzima.service.TimeoutPreferenceService;
 
 public class VideoWebViewActivity extends BaseHelpActivity {
 
-    private WebView webView;
-    private View mCustomView;
-    private WebChromeClient.CustomViewCallback customViewCallback;
     public static final String VIDEO_PATH = "VIDEO_PATH";
     public static final String VIDEO_TITLE = "VIDEO_TITLE";
+    private WebView webView;
     private int muzimaTimeout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,20 +49,17 @@ public class VideoWebViewActivity extends BaseHelpActivity {
     private void setVideoHelpContent() {
         setTitle(getIntent().getStringExtra(VIDEO_TITLE));
         webView = (WebView) findViewById(R.id.videoWebView);
-
-        webView.setWebViewClient(new myWebViewClient());
-
+        webView.setWebChromeClient(new WebChromeClient(){});
+        webView.setWebViewClient(new videoWebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setBackgroundColor(Color.TRANSPARENT);
-
-        String html = makeHtml();
-        webView.loadDataWithBaseURL("https://www.youtube.com", html, "text/html", "UTF-8", null);
+        webView.loadData(makeHtml(), "text/html", "utf-8");
     }
 
     private String makeHtml() {
         String videoId = getVideoId(getIntent().getStringExtra(VIDEO_PATH));
         String videoUrl = "https://www.youtube.com/embed/" + videoId;
-        String html = "<iframe src='" + videoUrl + "' frameborder='0' allowFullScreen></iframe>";
+        String html = "<html><head><iframe class='youtube-player' src='" + videoUrl + "' frameborder='0' allowFullScreen></iframe>";
         return html;
     }
 
@@ -81,21 +69,7 @@ public class VideoWebViewActivity extends BaseHelpActivity {
         return videoId;
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                super.onBackPressed();
-                Intent intent = new Intent(this, HelpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-        }
-        return true;
-    }
-
-    class myWebViewClient extends WebViewClient {
+    class videoWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             return super.shouldOverrideUrlLoading(view, url);
@@ -103,9 +77,6 @@ public class VideoWebViewActivity extends BaseHelpActivity {
 
         @Override
         public void onPageFinished(WebView view, String url) {
-            webView.getHeight();
-            webView.getWidth();
-
             super.onPageFinished(view, url);
             if (!isUserLoggedOut()) {
                 setTimer();
@@ -115,8 +86,7 @@ public class VideoWebViewActivity extends BaseHelpActivity {
         private void setTimer() {
             //set session timeout to 20 minutes
             int duration = 20;
-            int timeout = muzimaTimeout;
-            if (timeout < duration) {
+            if (muzimaTimeout < duration) {
                 ((MuzimaApplication) getApplication()).resetTimer(duration);
             }
         }
