4469f958940610e0c1b3555edc2d2aec5e57bf32
benamonya <benanyanga@gmail.com>
2019-02-12 15:45:39 +0300

    MUZIMA-699: Changed default landing page to client listing

--- app/src/main/java/com/muzima/service/LandingPagePreferenceService.java
+++ app/src/main/java/com/muzima/service/LandingPagePreferenceService.java
@@ -3,6 +3,7 @@ package com.muzima.service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import com.muzima.R;
 import com.muzima.utils.StringUtils;
@@ -21,11 +22,11 @@ public class LandingPagePreferenceService extends PreferenceService{
 
     public Intent getLandingPageActivityLauchIntent(){
         String landingPageKey = context.getResources().getString(R.string.preference_landing_page);
-        String defaultLandingPage = context.getString(R.string.general_dashboard);
+        String defaultLandingPage = context.getString(R.string.general_client_list);
         String preferredLandingPage = settings.getString(landingPageKey,defaultLandingPage);
         Intent intent;
         if(StringUtils.equals(preferredLandingPage,context.getString(R.string.general_client_list))){
-            intent = new Intent(context, PatientsListActivity.class);
+           intent = new Intent(context, PatientsListActivity.class);
         } else {
             intent = new Intent(context, MainActivity.class);
         }
