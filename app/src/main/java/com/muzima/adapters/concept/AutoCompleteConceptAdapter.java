cf41c17cace410ab4c984d774d7535786e001fd5
sthaiya <sthaiya@hotmail.com>
2019-11-11 12:52:34 +0300

    MUZIMA-794: Added filterComplete method on AutoCompleteBase Adapter

--- app/src/main/java/com/muzima/adapters/concept/AutoCompleteConceptAdapter.java
+++ app/src/main/java/com/muzima/adapters/concept/AutoCompleteConceptAdapter.java
@@ -45,5 +45,5 @@ public class AutoCompleteConceptAdapter extends AutoCompleteBaseAdapter<Concept>
     }
 
     @Override
-    protected void filterComplete() {}
+    protected void filterComplete(int count) {}
 }
