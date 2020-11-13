862e274cd4224b0acde2ab221047bb85600bb5b4
benamonya <benanyanga@gmail.com>
2020-11-13 08:21:48 +0300

    removed commented out code

--- app/src/test/java/com/muzima/adapters/forms/CompleteFormsAdapterTest.java
+++ app/src/test/java/com/muzima/adapters/forms/CompleteFormsAdapterTest.java
@@ -76,28 +76,6 @@ public class CompleteFormsAdapterTest {
         assertEquals(completeFormsWithPatientData, queryTask.get(100, TimeUnit.MILLISECONDS));
     }
 
-//    @Test
-//    public void shouldGroupPatients() throws FormController.FormFetchException {
-//        BackgroundQueryTask queryTask = new BackgroundQueryTask(formsAdapter);
-//        final Patient patient1 = patient("identifier1");
-//        final Patient patient2 = patient("identifier2");
-//        CompleteFormsWithPatientData completeFormsWithPatientData = new CompleteFormsWithPatientData() {{
-//            add(completeFormWithPatientData(patient1));
-//            add(completeFormWithPatientData(patient2));
-//            add(completeFormWithPatientData(patient1));
-//        }};
-//
-//        when(formController.getAllCompleteFormsWithPatientData(context)).thenReturn(completeFormsWithPatientData);
-//        StickyListHeadersListView listView = new StickyListHeadersListView(RuntimeEnvironment.application);
-//        listView.setAdapter(formsAdapter);
-//        formsAdapter.setListView(listView);
-//
-//        queryTask.execute();
-//        Robolectric.flushBackgroundThreadScheduler();
-//        ShadowLooper.runUiThreadTasks();
-//        assertThat(formsAdapter.getPatients(), is(asList(patient1, patient2)));
-//    }
-
     private CompleteFormWithPatientData completeFormWithPatientData(final Patient patient1) {
         return new CompleteFormWithPatientData() {{
             setPatient(patient1);
