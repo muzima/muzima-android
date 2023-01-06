fbb7eb1a781a920428b02b0b85ee900a8ee50f76
benamonya <benanyanga@gmail.com>
2023-01-06 12:46:23 +0300

    More on grid layout

--- app/src/main/java/com/muzima/adapters/media/MediaRecyclerViewAdapter.java
+++ app/src/main/java/com/muzima/adapters/media/MediaRecyclerViewAdapter.java
@@ -6,10 +6,13 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.pdf.PdfRenderer;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.muzima.R;
 import com.muzima.api.model.Media;
 import com.muzima.api.model.MediaCategory;
+
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -68,14 +73,14 @@ public class MediaRecyclerViewAdapter extends RecyclerView.Adapter<MediaRecycler
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
         Media media = (Media) getChild(groupPosition, position);
-        final String childText = media.getName();
-        holder.name.setText(childText);
+        final String mediaName = media.getName();
+        holder.name.setText(mediaName);
 
         String mimeType = media.getMimeType();
         String PATH = Objects.requireNonNull(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)).getAbsolutePath();
         File file = new File(PATH + "/"+media.getName()+"."+mimeType.substring(mimeType.lastIndexOf("/") + 1));
 
-        if(mimeType.substring(mimeType.lastIndexOf("/") + 1).equals("png")){
+        if(StringUtils.substringBefore(mimeType, "/").equals("image")){
             Uri uri = Uri.fromFile(file);
             Bitmap bitmap = null;
             try {
@@ -84,15 +89,18 @@ public class MediaRecyclerViewAdapter extends RecyclerView.Adapter<MediaRecycler
                 holder.imageView.setImageBitmap(thumbnail);
             } catch (IOException e) {
                 e.printStackTrace();
+                Bitmap thumbnail = getDefaultThumbNail();
+                holder.imageView.setImageBitmap(thumbnail);
             }
         }else if(mimeType.substring(mimeType.lastIndexOf("/") + 1).equals("mp4")){
             Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.MICRO_KIND);
             Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap,400,400);
             holder.imageView.setImageBitmap(thumbnail);
+        }else if(mimeType.substring(mimeType.lastIndexOf("/") + 1).equals("pdf")) {
+            Bitmap thumbnail = getPDFThumbnail(file);
+            holder.imageView.setImageBitmap(thumbnail);
         }else{
-            Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(),
-                    R.drawable.splash_background);
-            Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap,400,400);
+            Bitmap thumbnail = getDefaultThumbNail();
             holder.imageView.setImageBitmap(thumbnail);
         }
 
@@ -101,7 +109,6 @@ public class MediaRecyclerViewAdapter extends RecyclerView.Adapter<MediaRecycler
             @Override
             public void onClick(View view) {
                 startMediaDisplayActivity(media);
-                Toast.makeText(context, "Clicked on "+childText, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -132,5 +139,37 @@ public class MediaRecyclerViewAdapter extends RecyclerView.Adapter<MediaRecycler
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
             context.startActivity(intent);
         }
+    }
+
+    private Bitmap getDefaultThumbNail(){
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.splash_background);
+        Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap,400,400);
+        return thumbnail;
+    }
+
+    private Bitmap getPDFThumbnail(File PDFFile){
+        Bitmap bitmap = null;
+        try {
+            PdfRenderer renderer = new PdfRenderer(ParcelFileDescriptor.open(PDFFile, ParcelFileDescriptor.MODE_READ_ONLY));
+            final int pageCount = renderer.getPageCount();
+            if(pageCount>0){
+                PdfRenderer.Page page = renderer.openPage(0);
+                int width = context.getResources().getDisplayMetrics().densityDpi / 72 * page.getWidth();
+                int height = context.getResources().getDisplayMetrics().densityDpi / 72 * page.getHeight();
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+                page.close();
+            }else{
+                bitmap = getDefaultThumbNail();
+            }
+            renderer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            bitmap = getDefaultThumbNail();
+        }
+        Bitmap thumbnail = ThumbnailUtils.extractThumbnail(bitmap,400,400);
+        return thumbnail;
     }
 }
