bfa6ca433db484e0068fa575e78efd2c22475942
voloide <voloidet@gmail.com>
2024-01-25 15:23:49 +0200

    Section collapse and expand

--- app/src/main/java/com/muzima/utils/ViewUtil.java
+++ app/src/main/java/com/muzima/utils/ViewUtil.java
@@ -12,6 +12,10 @@ package com.muzima.utils;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 import com.muzima.MuzimaApplication;
 import com.muzima.controller.FormController;
@@ -45,5 +49,56 @@ public class ViewUtil {
                 }
             });
         }
+    }
+
+    public static void expand(View view) {
+        Animation animation = expandAction(view);
+        view.startAnimation(animation);
+    }
+
+    private static Animation expandAction(final View view) {
+
+        view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        final int actualheight = view.getMeasuredHeight();
+
+        view.getLayoutParams().height = 0;
+        view.setVisibility(View.VISIBLE);
+
+        Animation animation = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                view.getLayoutParams().height = interpolatedTime == 1 ? ViewGroup.LayoutParams.WRAP_CONTENT : (int) (actualheight * interpolatedTime);
+                view.requestLayout();
+            }
+        };
+
+        animation.setDuration((long) (actualheight / view.getContext().getResources().getDisplayMetrics().density));
+        view.startAnimation(animation);
+
+        return animation;
+
+
+    }
+
+    public static void collapse(final View view) {
+
+        final int actualHeight = view.getMeasuredHeight();
+
+        Animation animation = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+
+                if (interpolatedTime == 1) {
+                    view.setVisibility(View.GONE);
+                } else {
+                    view.getLayoutParams().height = actualHeight - (int) (actualHeight * interpolatedTime);
+                    view.requestLayout();
+
+                }
+            }
+        };
+
+        animation.setDuration((long) (actualHeight/ view.getContext().getResources().getDisplayMetrics().density));
+        view.startAnimation(animation);
     }
 }
