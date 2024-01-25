2f324e5e9b802aba018dbec1a21a0ebd9330586a
voloide <voloidet@gmail.com>
2024-01-25 11:29:00 +0200

    Estrutura do modulo ATS

---
+++ app/src/main/java/com/muzima/view/person/PersonRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.muzima.view.person;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muzima.R;
+
+public class PersonRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_register);
+    }
+}
