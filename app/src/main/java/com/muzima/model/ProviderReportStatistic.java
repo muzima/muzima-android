b314400a5accd3b47db05c4312666f2c8db7e748
mssavai <mssavai@gmail.com>
2024-09-24 11:07:16 +0300

    Add report search

--- app/src/main/java/com/muzima/model/ProviderReportStatistic.java
+++ app/src/main/java/com/muzima/model/ProviderReportStatistic.java
@@ -14,6 +14,7 @@ public class ProviderReportStatistic implements Comparable{
     private int leaderboardColor;
     private String  startDate;
     private String  endDate;
+    private boolean isVisible = true;
 
     public int getAchievement() {
         return achievement;
@@ -117,6 +118,14 @@ public class ProviderReportStatistic implements Comparable{
 
     public String getEndDate() {
         return endDate;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
     }
 
     @Override
--- app/src/main/java/com/muzima/view/reports/LeaderboardFragment.java
+++ app/src/main/java/com/muzima/view/reports/LeaderboardFragment.java
@@ -1,9 +1,12 @@
 package com.muzima.view.reports;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.muzima.R;
 import com.muzima.adapters.reports.LeaderboardAdapter;
+import com.muzima.view.custom.MuzimaRecyclerView;
 
 public class LeaderboardFragment extends Fragment {
     private LeaderboardAdapter leaderboardAdapter;
@@ -31,9 +35,29 @@ public class LeaderboardFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        RecyclerView leaderboardView = view.findViewById(R.id.leaderboard);
+        MuzimaRecyclerView leaderboardView = view.findViewById(R.id.leaderboard);
         leaderboardView.setLayoutManager(new LinearLayoutManager(requireActivity().getApplicationContext()));
+        leaderboardView.setNoDataLayout(view.findViewById(R.id.no_data_layout),
+                getString(R.string.info_no_provider_reports_found),
+                getString(R.string.hint_no_provider_reports_available));
         leaderboardView.setAdapter(leaderboardAdapter);
         leaderboardAdapter.notifyDataSetChanged();
+
+        EditText searchView = view.findViewById(R.id.provider_search);
+        searchView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                leaderboardAdapter.filterByText(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
